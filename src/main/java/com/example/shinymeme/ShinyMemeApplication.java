2d6486fe63e326d20a22ef88fc61c178b9f8c987
jeninalanzi <jeninalanzi@gmail.com>
2019-08-15 09:34:12 -0500

    First commit

---
+++ src/main/java/com/example/shinymeme/ShinyMemeApplication.java
@@ -0,0 +1,13 @@
+package com.example.shinymeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinyMemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinyMemeApplication.class, args);
+	}
+
+}
